231d60c7c0611f7698ad53e33ad644b11fe67c5e
AnastasiaLL <lukyanchik.nastya@bk.ru>
2022-10-22 19:16:49 +0300

    fix: devServer config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const EslingPlugin = require('eslint-webpack-plugin');
-////////////////////////////////////////////////////////////
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
@@ -11,51 +9,42 @@ const baseConfig = {
     entry: path.resolve(__dirname, './src/index.js'),
     mode: 'development',
     module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            // {
-            //     test: /\.[tj]s$/,
-            //     use: 'ts-loader',
-            //     exclude: /node_modules/,
-            // },
+      rules: [
+          // {
+          //     test: /\.css$/i,
+          //     use: ['style-loader', 'css-loader'],
+          // },
 
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
-                type: 'asset/resource',
-            },
+          {
+              test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
+              type: 'asset/resource',
+          },
 
-            ////////////////////        all this add:
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
+          {
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
 
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                //   // Creates `style` nodes from JS strings
-                //   "style-loader",
-                //   // Translates CSS into CommonJS
-                //   "css-loader",
-                //   // Compiles Sass to CSS
-                  "sass-loader",
-                ],
-            },
+          {
+              test: /\.s[ac]ss$/i,
+              use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader",
+              ],
+          },
 
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
+          {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
                 }
-            },
-        ],
+              }
+          },
+      ],
     },
     resolve: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
@@ -66,31 +55,32 @@ const baseConfig = {
         assetModuleFilename: '[file]',
     },
 
-                       //add
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
-        // new EslingPlugin({ extensions: 'ts' }),
-
-        new MiniCssExtractPlugin(),     //add
+        new MiniCssExtractPlugin(),  
         
     ],
+
+    performance: {
+      hints: false,
+    },
  
-    optimization: {         //add
+    optimization: {         
         minimizer: [
           new CssMinimizerPlugin(),
         ],
-        runtimeChunk: 'single',         //add
+        runtimeChunk: 'single',         
     }    
 };
 
 module.exports = ({ mode }) => {
 
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
-    return merge(baseConfig, envConfig);
+  return merge(baseConfig, envConfig);
 };
